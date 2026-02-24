31c22fd7871274d7346607c1bf6ad482ca542821
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2026-02-24 22:37:32 +0100

    add supabase email login and signup

---
+++ js/auth.js
@@ -0,0 +1,80 @@
+import { supabase, getCurrentSession } from "./supabase.js";
+
+//Signup funtion
+async function signup(email, password) {
+   const {data, error} = await supabase.auth.signUp({
+      email, 
+      password,
+   })
+
+   if(error) {
+      alert(error.message)
+      return
+   }
+   alert("Account created successfully!")
+}
+
+
+//Signup form
+const signupForm = document.getElementById("signupForm");
+signupForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("userSignupEmailInput").value.trim();
+  const password = document
+    .getElementById("userSignupPasswordInput")
+    .value.trim();
+  const confirmPassword = document.getElementById(
+  "userSignupConfirmPasswordInput",
+).value.trim();
+
+  if (!password || !email || !confirmPassword) {
+    alert("All filed must not be empty");
+    return;
+  } else if (password != confirmPassword) {
+    alert("Passords do not match");
+    return;
+  }
+
+  await signup(email, password);
+});
+
+
+//login funtion
+async function login(email, password) {
+   const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        alert(error.message);
+        return;
+      }
+      alert("Logged in successfully!");
+}
+
+//Login form
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+   e.preventDefault()
+
+  const email = document.getElementById("userLoginEmailInput").value.trim();
+  const password = document
+    .getElementById("userLoginPasswordInput")
+    .value.trim();
+
+  if (!password || !email) {
+    alert("All filed must not be empty");
+    return;
+  }
+
+  await login(email, password);
+})
+
+//REDIRECT IF USER LOGGED IN
+const session = getCurrentSession()
+if(session) {
+   window.location.href = "dashboard.html"
+}
---
+++ js/supabase.js
@@ -0,0 +1,23 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const SUPABASE_URL = "https://qqactsebaxdottiiyrng.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_fWWIiWF4l_q-eNHU-Rs5qQ_zlaMggjo";
+
+//EXPORT CLIENT INFO
+export const supabase = createClient(
+   SUPABASE_URL, SUPABASE_ANON_KEY
+)
+
+
+// Reactive session storage
+let currentSession = null;
+
+//GET SESSION
+supabase.auth.onAuthStateChange((event, session) => {
+  currentSession = session
+});
+
+// Function other files can use
+export function getCurrentSession() {
+  return currentSession;
+}
