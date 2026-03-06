c8a155f196f8454cc9a881167445c9efdbe3a75b
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2026-03-06 08:08:18 +0100

    try: fix oauth bug

--- js/utils/noAuthSessionProtect.js
+++ js/utils/noAuthSessionProtect.js
@@ -1,11 +1,13 @@
 import { supabase } from "../supabase.js";
 
 async function protectPage() {
+  /*
   supabase.auth.onAuthStateChange((event, session) => {
     if (!session) {
       window.location.href = "https://scyflix.github.io/LogHue/pages/auth.html";
     }
   });
+*/
 
   const {
     data: { session },
