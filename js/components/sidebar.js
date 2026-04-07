52781482300b601692ebeabb82edaf2b4bfd041f
scybudtech <scybudtech@gmail.com>
2026-04-07 20:40:35 +0200

    try:fix sidebatr dropdown bug

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -25,11 +25,9 @@ function navDropdowns() {
    const navBtnDropdown = document.getElementById("navBtnDropdown");
    const historyNavs = document.querySelector(".historyNavs");
 
-   if (navBtnDropdown && historyNavs) {
      navBtnDropdown.addEventListener("click", () => {
        navBtnDropdown.classList.toggle("dropped");
 
        historyNavs.classList.toggle("dropNav");
      });
-   }
 }
