5d3099f223dd0dc461db49f3378948cf8e23c2e0
scybudtech <scybudtech@gmail.com>
2026-04-24 12:49:49 +0200

    fix: update redirect URL in noAuthSessionProtect to include original location

--- js/utils/noAuthSessionProtect.js
+++ js/utils/noAuthSessionProtect.js
@@ -2,9 +2,10 @@ import { sessionState, sessionReady } from "../session.js";
 
 async function protectAppPage() {
   await sessionReady;
+const redirectUrl = window.location.href;
 
   if (!sessionState.user) {
-    window.location.href = "/auth";
+    window.location.href = `/auth?redirect=${redirectUrl}`;
     return;
   }
 
