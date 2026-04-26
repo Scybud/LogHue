47de49655fa5a9794ca659bfabc2decb399a7f07
scybudtech <scybudtech@gmail.com>
2026-04-26 22:56:16 +0200

    add align to notes toolbar

--- js/features/notes.js
+++ js/features/notes.js
@@ -63,8 +63,12 @@ function initNotes() {
       toolbar: [
         [{ header: [1, 2, 3, 4, 5, 6, false] }],
         ["bold", "italic", "underline"],
-        [{ list: "ordered" }, { list: "bullet" }],
-        ["link"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { list: "check" },
+          { align: [] },
+        ],
         ["image"],
         ["code-block"],
       ],
