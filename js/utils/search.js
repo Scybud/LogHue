8efa61008dee9b1ac0048b1d0a4226dd460c9a2d
scybudtech <scybudtech@gmail.com>
2026-05-08 18:16:12 +0200

    feat: enhance search functionality with improved input handling and results display

--- js/utils/search.js
+++ js/utils/search.js
@@ -1,4 +1,9 @@
 import { supabase } from "../supabase.js";
+import {sessionReady, sessionState} from "../session.js"
+
+await sessionReady;
+
+const user = sessionState.user;
 
 const searchInput = document.getElementById("searchInput");
 const resultsContainer = document.getElementById("searchResults");
@@ -7,23 +12,75 @@ searchInput.addEventListener("input", async (e) => {
   const value = e.target.value.trim();
 
   // Prevent empty searches
-  if (!value) {
+  if (!value || value.length < 3) {
     resultsContainer.innerHTML = "";
     return;
   }
 
-  const { data, error } = await supabase
-    .from("workspaces")
-    .select("id, name")
-    .ilike("name", `%${value}%`)
-    .limit(10);
+const { data: workspaceSearch, error: workspaceSearchError } = await supabase
+  .from("workspace_members")
+  .select(
+    `
+    role,
+    workspaces: workspace_id (
+      id,
+      name
+    )
+  `,
+  )
+  .eq("user_id", user.id)
+  .ilike("workspaces.name", `%${value}%`)
+  .limit(10);
 
-  if (error) {
-    console.error(error);
+
+
+      if (workspaceSearchError) {
+        console.error(workspaceSearchError);
+        return;
+      }
+
+      const { data: tasksSearch, tasksSearchError } = await supabase
+        .from("workspace_tasks")
+        .select("id, title")
+        .ilike("title", `%${value}%`)
+        .limit(10);
+
+        if (workspaceSearchError || tasksSearchError) {
+          console.error(workspaceSearchError || tasksSearchError);
+          return;
+        }
+
+
+const workspaceSearchTagged = workspaceSearch
+  .filter((w) => w.workspaces) // prevent undefined
+  .map((w) => ({
+    id: w.workspaces.id,
+    name: w.workspaces.name,
+    type: "workspace",
+    role: w.role,
+  }));
+
+
+const tasksSearchTagged = tasksSearch.map((t) => ({
+  id: t.id,
+  title: t.title,
+  type: "task",
+}));
+
+
+     const searchData = [
+  ...workspaceSearchTagged,
+  ...tasksSearchTagged
+];
+
+
+  if (tasksSearchError) {
+    console.error(tasksSearchError);
     return;
   }
 
-  renderResults(data);
+
+  renderResults(searchData);
 });
 
 function renderResults(results) {
@@ -34,13 +91,28 @@ function renderResults(results) {
     return;
   }
 
-  results.forEach((workspace) => {
+  results.forEach((result) => {
+
+    const link =
+      result.type === "workspace"
+        ? result.role === "admin" || result.role === "owner"
+          ? `workspace-dashboard-admin?id=${result.id}`
+          : `workspace-dashboard-member?id=${result.id}`
+        : `task-view?id=${result.id}`;
+
+        
     const div = document.createElement("div");
 div.classList.add("searchItem");
 
-    div.innerHTML = `
-            <h4>${workspace.name}</h4>
-    `;
+div.innerHTML = `
+  <a href="${link}">
+    <div class="searchType ${result.type}">
+      ${result.type === "workspace" ? "Workspace" : "Task"}
+    </div>
+    <h4>${result.name || result.title}</h4>
+  </a>
+`;
+
 
     resultsContainer.appendChild(div);
   });
