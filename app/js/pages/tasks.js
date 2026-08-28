b9d4cf3a4f9d684f4ca36f0279137dce43539d56
scybudtech <scybudtech@gmail.com>
2026-08-28 19:10:35 +0200

    feat: add skeleton loading UI for tasks and sidebar components

--- app/js/pages/tasks.js
+++ app/js/pages/tasks.js
@@ -1,3 +1,18 @@
 import { initPersonalTasks } from "./personalTasks.js";
 
+const tasksSkeleton = document.getElementById("tasksSkeleton");
+const personalCreatedTasks = document.getElementById("personalCreatedTasks");
+
+function showTasksSkeleton() {
+  tasksSkeleton?.removeAttribute("hidden");
+  personalCreatedTasks?.setAttribute("hidden", "");
+}
+
+function hideTasksSkeleton() {
+  tasksSkeleton?.setAttribute("hidden", "");
+  personalCreatedTasks?.removeAttribute("hidden");
+}
+
 initPersonalTasks();
+
+hideTasksSkeleton();
